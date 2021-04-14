11903d451b0e9bcab354dfa8a9ba8887ccf1fa8e
Alam Sierra <alam.15@outlook.com>
2021-04-13 21:11:16 -0400

    new footer

--- sources/js/index.js
+++ sources/js/index.js
@@ -1,14 +1,14 @@
-const nav = document.querySelector("nav");
-const navTop = nav.offsetTop;
+// const nav = document.querySelector("nav");
+// const navTop = nav.offsetTop;
 
-window.addEventListener("scroll", function (e) {
-  // console.log({ windowScrollY: window.scrollY, navTop });
-  if (window.scrollY > navTop) {
-    nav.classList.add("sticky");
-  } else {
-    nav.classList.remove("sticky");
-  }
-});
+// window.addEventListener("scroll", function (e) {
+//   console.log({ windowScrollY: window.scrollY, navTop });
+//   if (window.scrollY > navTop) {
+//     nav.classList.add("sticky");
+//   } else {
+//     nav.classList.remove("sticky");
+//   }
+// });
 
 const date = new Date();
 let currentYear = date.getFullYear();
