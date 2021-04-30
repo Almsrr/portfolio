f9771e2edc0cc44ff82331a3e8988f3f7b94d3b5
Alam Sierra <alam.15@outlook.com>
2021-04-29 23:17:31 -0400

    animation to dev title

--- sources/js/index.js
+++ sources/js/index.js
@@ -1,25 +1,3 @@
-// const nav = document.querySelector("nav");
-// const navTop = nav.offsetTop;
-
-// window.addEventListener("scroll", function (e) {
-//   console.log({ windowScrollY: window.scrollY, navTop });
-//   if (window.scrollY > navTop) {
-//     nav.classList.add("sticky");
-//   } else {
-//     nav.classList.remove("sticky");
-//   }
-// });
-
-// Navbar mobile button
-// function myFunction() {
-//   var links = document.getElementById("links");
-//   if (links.style.display === "block") {
-//     links.style.display = "none";
-//   } else {
-//     links.style.display = "block";
-//   }
-// }
-
 // Copyright date
 const date = new Date();
 let currentYear = date.getFullYear();
@@ -28,7 +6,16 @@ document.getElementById("copyright").innerHTML = "&copy" + currentYear;
 
 // Mobile navbar links button
 $(document).ready(function() {
-  $("#mobile-btn").click(function() {
+  $("#mobile-btn").on("click", function() {
     $("#links").slideToggle();
   });
+});
+
+// Front-end title animation with typed.js
+$(document).ready(function() {
+  var options = {
+    strings: ["frontend", "Front-End </br>Web Developer"],
+    typeSpeed: 50
+  };
+  var typed = new Typed("#typed", options);
 });
