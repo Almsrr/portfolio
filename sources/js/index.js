4d8d54132618d96da9553832301d48be10372230
Alam Sierra <alam.15@outlook.com>
2021-05-31 15:28:24 -0400

    add new technologies

--- sources/js/index.js
+++ sources/js/index.js
@@ -5,17 +5,17 @@ let currentYear = date.getFullYear();
 document.getElementById("copyright").innerHTML = "&copy" + currentYear;
 
 // Mobile navbar links button
-$(document).ready(function() {
-  $("#mobile-btn").on("click", function() {
+$(document).ready(function () {
+  $("#mobile-btn").on("click", function () {
     $("#links").slideToggle();
   });
 });
 
 // Front-end title animation with typed.js
-$(document).ready(function() {
+$(document).ready(function () {
   var options = {
     strings: ["frontend", "Front-End </br>Web Developer"],
-    typeSpeed: 50
+    typeSpeed: 50,
   };
   var typed = new Typed("#typed", options);
 });
