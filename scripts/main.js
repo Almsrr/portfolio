e4f7de798c5e1d0ec0f152c28a898ce4aca2e8a7
Alam Sierra <alam.15@outlook.com>
2021-06-28 17:21:41 -0400

    header

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,21 @@
 // Copyright date
-const date = new Date();
-let currentYear = date.getFullYear();
+// const date = new Date();
+// let currentYear = date.getFullYear();
 
-document.getElementById("copyright").innerHTML = "&copy" + currentYear;
+// document.getElementById("copyright").innerHTML = "&copy" + currentYear;
 
 // Mobile navbar links button
-$(document).ready(function () {
-  $("#mobile-btn").on("click", function () {
-    $("#links").slideToggle();
-  });
-});
+// $(document).ready(function () {
+//   $("#mobile-btn").on("click", function () {
+//     $("#links").slideToggle();
+//   });
+// });
 
 // Front-end title animation with typed.js
-$(document).ready(function () {
-  var options = {
-    strings: ["frontend", "Front-End </br>Web Developer"],
-    typeSpeed: 50,
-  };
-  var typed = new Typed("#typed", options);
-});
+// $(document).ready(function () {
+//   var options = {
+//     strings: ["frontend", "Front-End </br>Web Developer"],
+//     typeSpeed: 50,
+//   };
+//   var typed = new Typed("#typed", options);
+// });
