d06441da8e79346dcfc40027cfdc326f5c077f7a
Alam <alam.15@outlook.com>
2021-07-22 22:41:41 -0400

    mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -6,13 +6,6 @@ window.onload = function () {
   document.getElementById("copyright").innerHTML = "&copy" + currentYear;
 };
 
-// Mobile navbar links button
-// $(document).ready(function () {
-//   $("#mobile-btn").on("click", function () {
-//     $("#links").slideToggle();
-//   });
-// });
-
 // Front-end title animation with typed.js
 // $(document).ready(function () {
 //   var options = {
@@ -21,3 +14,16 @@ window.onload = function () {
 //   };
 //   var typed = new Typed("#typed", options);
 // });
+
+var bodyWrapper = document.getElementById("body-wrapper");
+
+var burgerBtn = bodyWrapper.getElementsByClassName("burger-btn")[0];
+var contentWrapper = bodyWrapper.getElementsByClassName("content-wrapper")[0];
+
+burgerBtn.addEventListener(
+  "click",
+  function () {
+    bodyWrapper.classList.toggle("navigation");
+  },
+  false
+);
