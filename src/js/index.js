22bd6389865ced2a5611e8abdfeb2c0fc5bf88c8
Alam <alam.15@outlook.com>
2021-10-04 12:46:52 -0400

    sticky  navbar

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import bootstrap from "bootstrap";
 import AOS from "aos";
+import $ from "jquery";
 
 import "../scss/main.scss";
 import "aos/dist/aos.css";
@@ -8,8 +9,30 @@ AOS.init({
   once: true,
 });
 
+// Copyright
 const date = new Date();
 const currentYear = date.getFullYear();
 
 const copy = document.getElementById("copyright");
 copy.innerHTML = `&copy;${currentYear}`;
+
+// Navbar
+const navbar = document.getElementById("nav");
+const sticky = navbar.offsetTop;
+const pageContent = document.querySelector("main");
+
+const stickyNavbar = () => {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky");
+    pageContent.style.paddingTop = `${navbar.offsetHeight}px`;
+  } else {
+    navbar.classList.remove("sticky");
+    pageContent.style.paddingTop = "0";
+  }
+};
+
+$(document).ready(() => {
+  $(window).scroll(() => {
+    stickyNavbar();
+  });
+});
