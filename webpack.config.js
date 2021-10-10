f27527884606ede44fc63841630c3fb4868ddda8
Alam <alam.15@outlook.com>
2021-10-10 03:08:31 -0400

    some webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,59 @@
 const path = require("path");
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/js/index.js",
-  output: {
-    filename: "index.bundle.js",
-    path: path.resolve(__dirname, "public"),
+  entry: {
+    main: "./src/js/index.js",
   },
-  devServer: {
-    static: "./",
-    port: 3000,
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "./build"),
   },
   module: {
     rules: [
       {
-        test: /\.m?js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
           "css-loader",
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: function () {
-                  return [require("autoprefixer")];
-                },
-              },
-            },
-          },
+          "postcss-loader",
           "sass-loader",
         ],
       },
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
         test: /\.html$/i,
         loader: "html-loader",
+        options: {
+          sources: true,
+        },
       },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: "index.html" }),
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
     new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
   ],
   optimization: {
-    minimizer: [new CssMinimizerPlugin()],
+    minimize: true,
+    minimizer: ["...", new CssMinimizerPlugin()],
+  },
+  devServer: {
+    static: "./",
+    port: 3000,
   },
 };
