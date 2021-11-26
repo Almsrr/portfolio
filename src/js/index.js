661e3ca8f8f63e0882dddd2af507e77e877a8692
Alam <alam.15@outlook.com>
2021-11-26 06:08:10 -0400

    tooltips

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,5 @@
-import { popper } from "@popperjs/core";
-import { Collapse, Tooltip } from "bootstrap";
+import Tooltip from "bootstrap/js/dist/tooltip";
 import AOS from "aos";
-import $ from "jquery";
 
 import "../scss/main.scss";
 import "aos/dist/aos.css";
@@ -17,47 +15,10 @@ const currentYear = date.getFullYear();
 const copy = document.getElementById("copyright");
 copy.innerHTML = `&copy;${currentYear}`;
 
-// Navbar
-const navbar = document.getElementById("nav");
-const sticky = navbar.offsetTop;
-const pageContent = document.querySelector("main");
-const breakpoint = 768;
-let screenResolution = 0;
-
-const stickyNavbar = () => {
-  if (screenResolution >= breakpoint) {
-    if (window.scrollY >= sticky) {
-      navbar.classList.add("sticky");
-      pageContent.style.paddingTop = `${navbar.offsetHeight}px`;
-    } else {
-      navbar.classList.remove("sticky");
-      pageContent.style.paddingTop = "0";
-    }
-  }
-};
-
-const getResolution = () => {
-  screenResolution = window.screen.availWidth;
-};
-
-$(document).ready(() => {
-  getResolution();
-
-  $(window).on("scroll", () => {
-    stickyNavbar();
-  });
-
-  // Recalculate screenResolution var when screen resolution change
-  // (For example, portrait to landscape)
-  $(window).on("resize", () => {
-    getResolution();
-  });
-});
-
 // Tooltips
-const socialLinks = document.getElementsByClassName("social-link");
+const socialLinks = document.getElementsByClassName("socials__link");
 for (let i = 0; i < socialLinks.length; i++) {
-  const tooltip = new Tooltip(socialLinks[i], {
+  new Tooltip(socialLinks[i], {
     placement: "bottom",
   });
 }
