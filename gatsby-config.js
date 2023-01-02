0741b117624dfcf9085d857f3dcf5be6a930c4db
Alam Sierra <sierra_alam@outlook.com>
2023-01-01 20:03:40 -0400

    update config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,15 +3,30 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: "Alam Sierra's Portfolio",
+    siteUrl: "https://www.alamsierra.com",
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        "name": "images",
+        "path": "./src/images/",
+      },
+      __key: "images",
     },
-    __key: "images"
-  }]
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        "name": "pages",
+        "path": "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
 };
