838a273dc8f6da07ced35c95f6e28692fa5d28a4
Alam Sierra <sierra_alam@outlook.com>
2023-04-08 13:49:24 -0400

    Refactor navbar links and rename listener funct

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,23 @@ import classNames from "classnames";
 
 import { useSiteContext } from "../hooks";
 import ThemeSwitchButton from "./ThemeSwitchButton";
+import { generateId } from "../utils";
 
-const NavbarLink = ({ children, to }) => {
-  const urlArray = String(window.location.href).split("#");
-  const fragment = urlArray[urlArray.length - 1]; // the last fragment in the URL is the target
-  const linkFragment = to.replace("#", ""); // the fragment in the "to" prop
+const links = [
+  { href: "#home", title: "Home" },
+  { href: "#services", title: "Services" },
+  { href: "#projects", title: "Projects" },
+  { href: "#stack", title: "Stack" },
+  { href: "#contact", title: "Contact" },
+];
 
+const NavbarLink = ({ children, href, isActive }) => {
   const linkClasses = classNames("navbar-link", {
-    "active": fragment === linkFragment,
+    "active": isActive,
   });
 
   return (
-    <Link to={to} className={linkClasses}>
+    <Link to={href} className={linkClasses}>
       {children}
     </Link>
   );
@@ -28,12 +33,14 @@ export default function Navbar() {
   const lastScrollTop = useRef(0);
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll, { passive: true });
+    window.addEventListener("scroll", handleNavbarVisibility, {
+      passive: true,
+    });
 
-    return () => window.removeEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleNavbarVisibility);
   }, []);
 
-  const handleScroll = () => {
+  const handleNavbarVisibility = () => {
     const { pageYOffset } = window;
     if (pageYOffset > lastScrollTop.current) {
       setIsVisble(false);
@@ -45,11 +52,18 @@ export default function Navbar() {
     lastScrollTop.current = pageYOffset;
   };
 
+  const isLinkActive = (href) => {
+    const urlArray = String(window.location.href).split("#");
+    const fragment = urlArray[urlArray.length - 1]; // the last fragment in the URL is the target
+    const linkFragment = href.replace("#", ""); // the fragment in the "href" prop
+
+    return fragment === linkFragment;
+  };
+
   const linksClassName = classNames("links", {
     "light": !isDarkThemeActive,
     "dark": isDarkThemeActive,
   });
-
   const navbarClassName = classNames("site-navbar", { "visible": isVisible });
 
   return (
@@ -69,11 +83,15 @@ export default function Navbar() {
         </div>
         <div className="block links-block">
           <div className={linksClassName}>
-            <NavbarLink to="#home">Home</NavbarLink>
-            <NavbarLink to="#services">Services</NavbarLink>
-            <NavbarLink to="#projects">Projects</NavbarLink>
-            <NavbarLink to="#stack">Stack</NavbarLink>
-            <NavbarLink to="#contact">Contact</NavbarLink>
+            {links.map(({ href, title }) => (
+              <NavbarLink
+                key={generateId()}
+                href={href}
+                isActive={isLinkActive(href)}
+              >
+                {title}
+              </NavbarLink>
+            ))}
           </div>
         </div>
         <div className="block actions-block">
