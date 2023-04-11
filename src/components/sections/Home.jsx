851ef81ec3c843ce27d67daab7d44200c8b5918f
Alam Sierra <sierra_alam@outlook.com>
2023-04-11 18:24:44 -0400

    Add social links in home section

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
+import classNames from "classnames";
+
+import { useSiteContext } from "../../hooks";
 
 export default function HomeSection() {
+  const { isDarkThemeActive } = useSiteContext();
+
+  const linkClassName = classNames("socials__links", {
+    "light": !isDarkThemeActive,
+    "dark": isDarkThemeActive,
+  });
   return (
     <section className="home" id="home">
       <div className="home__container">
@@ -19,6 +28,43 @@ export default function HomeSection() {
               forget your name, and then have the deepest programming
               conversation of your life the following day.
             </p>
+            <div className="socials">
+              <h3>Find me on</h3>
+              <div className={linkClassName}>
+                <a
+                  href="https://www.linkedin.com/in/alam-sierra-6b49ab219/"
+                  target="_blank"
+                  title="LinkedIn"
+                  rel="noreferrer"
+                >
+                  <i className="bx bxl-linkedin" />
+                </a>
+                <a
+                  href="https://github.com/Almsrr"
+                  target="_blank"
+                  title="Github"
+                  rel="noreferrer"
+                >
+                  <i className="bx bxl-github" />
+                </a>
+                <a
+                  href="https://discord.com/"
+                  target="_blank"
+                  title="Discord"
+                  rel="noreferrer"
+                >
+                  <i className="bx bxl-discord-alt" />
+                </a>
+                <a
+                  href="https://www.instagram.com/alamsierrad/"
+                  target="_blank"
+                  title="Instagram"
+                  rel="noreferrer"
+                >
+                  <i className="bx bxl-instagram" />
+                </a>
+              </div>
+            </div>
           </div>
           <div className="hero__img-container">
             <StaticImage
