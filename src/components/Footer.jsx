03aaf4a5c8a380414f7478bf7df33fa84eb15aa5
Alam Sierra <sierra_alam@outlook.com>
2023-04-29 23:06:25 -0400

    Add source code link to footer and some styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,19 @@ export default function Footer() {
   return (
     <footer className="site-footer">
       <div className="container">
-        <p>2023</p>
-        <p>Made by Alam</p>
+        <div>
+          <p>&copy;2023</p>
+          <a
+            className="author"
+            href="https://github.com/almsrr/portfolio"
+            target="_blank"
+          >
+            <i className="code bx bx-code-curly" />
+            with
+            <i className="heart bx bxs-heart" />
+            by Alam
+          </a>
+        </div>
       </div>
     </footer>
   );
