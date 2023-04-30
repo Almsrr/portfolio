7955638a3858b4e65eacbea70bb10fd851902a2f
Alam Sierra <sierra_alam@outlook.com>
2023-04-29 21:44:17 -0400

    Contact form component

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+import { useForm } from "react-hook-form";
+import classNames from "classnames";
+
+export default function ContactForm({ isDarkTheme, onSubmit }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const formClassName = classNames("contact-form", {
+    "light": !isDarkTheme,
+    "dark": isDarkTheme,
+  });
+
+  return (
+    <form className={formClassName} onSubmit={handleSubmit(onSubmit)}>
+      <div className="input-container">
+        <div className="control name-control">
+          <label htmlFor="name">Name</label>
+          <input
+            name="name"
+            type="text"
+            placeholder="Marcelo Erizo"
+            {...register("name", { required: "The name is required" })}
+          />
+          {errors.name && (
+            <p className="error-message">{errors.name.message}</p>
+          )}
+        </div>
+        <div className="control email-control">
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            type="email"
+            placeholder="marceloerizo@example.com"
+            {...register("email", {
+              required: "The email is required",
+            })}
+          />
+          {errors.email && (
+            <p className="error-message">{errors.email.message}</p>
+          )}
+        </div>
+      </div>
+      <div className="control message-control">
+        <label htmlFor="message">Message</label>
+        <textarea
+          name="message"
+          placeholder="Type your message here..."
+          {...register("message", { required: "The message is required" })}
+          // rows={18}
+        />
+        {errors.message && (
+          <p className="error-message">{errors.message.message}</p>
+        )}
+      </div>
+      <button type="submit">Send Message</button>
+    </form>
+  );
+}
