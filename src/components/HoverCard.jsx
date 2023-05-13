24bcc0171e0670504810436e5be23c722bcb968e
Alam Sierra <sierra_alam@outlook.com>
2023-05-13 11:52:40 -0400

    Query of technologies

--- src/components/HoverCard.jsx
+++ src/components/HoverCard.jsx
@@ -1,26 +1,24 @@
 import React from "react";
+
 import classNames from "classnames";
-// import { StaticImage } from "gatsby-plugin-image";
 
 import Card from "./Card";
 
-export default function HoverCard({ imgSrc, title, description, className }) {
+export default function HoverCard({ image, title, description, className }) {
   const cardClassName = classNames("hover-card", className);
 
   return (
     <Card className={cardClassName}>
       <div className="body">
-        {/* <StaticImage
-          src=""
-          alt="stack-icon"
-          imgClassName="img"
-          placeholder="dominantColor"
-          width={80}
-          height={80}
-          layout="fixed"
-        /> */}
+        <div>
+          <img
+            src={image.file.url}
+            alt={image.filename}
+            className="hover-card__img"
+          />
+        </div>
         <h3>{title}</h3>
-        <div className="description">
+        <div className="hover-card__description">
           <span className="line"></span>
           <p>{description}</p>
         </div>
