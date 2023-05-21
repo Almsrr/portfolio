7c3e911e3e2accf4dadd4b21e87297e50a208f3d
Alam Sierra <sierra_alam@outlook.com>
2023-05-21 16:44:24 -0400

    Use composition instead of Gatsby apis
    Context provider and Layout

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,11 +4,11 @@ require("boxicons/css/boxicons.min.css");
 require("./src/sass/main.scss");
 require("@fortawesome/fontawesome-free/css/all.min.css");
 require("aos/dist/aos.css");
-const api = require("./gatsby-api-methods");
 const AOS = require("aos");
+// const api = require("./gatsby-api-methods");
 
 exports.onInitialClientRender = () => AOS.init();
 
-exports.wrapPageElement = api.wrapWithLayout;
+// exports.wrapPageElement = api.wrapWithLayout;
 
-exports.wrapRootElement = api.wrapWithProvider;
+// exports.wrapRootElement = api.wrapWithProvider;
