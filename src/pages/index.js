35aed73332a0f70622a10f4ef882d2cdd1c41dc5
Alam Sierra <sierra_alam@outlook.com>
2023-06-04 13:42:53 -0400

    Uncomment stack section

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,8 +13,8 @@ export default function IndexPage() {
       <HomeSection />
       <ServicesSection />
       <ProjectsSection />
-      {/* <StackSection />
-      <ContactSection /> */}
+      <StackSection />
+      {/* <ContactSection /> */}
     </>
   );
 }
